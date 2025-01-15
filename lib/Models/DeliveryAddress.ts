f507c7d8fb31d1c97b01cba095dbc4b7c158fe20
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-01-15 19:42:21 +0530

    Added delivery addresses model

---
+++ lib/Models/DeliveryAddress.ts
@@ -0,0 +1,12 @@
+import { Schema, model, models } from 'mongoose'
+
+const addressSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    place: { type: String, required: true, enum: ['Home', 'Work Place', 'Instution', 'Undifined'] },
+    no: { type: Number, required: false },
+    street: { type: String, required: false },
+    town: { type: String, requrired: true },
+    division: { type: String, required: true },
+    district: { type: String, required: true },
+    contactNumber: { type: Number, required: true }
+});
